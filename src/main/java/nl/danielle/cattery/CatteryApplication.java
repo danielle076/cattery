ed85797f7bd7aa5c95cfda6ac9cb55fdf362a681
danielle076 <d.vandenakker@novi-education.nl>
2021-10-26 11:52:37 +0200

    first commit

---
+++ src/main/java/nl/danielle/cattery/CatteryApplication.java
@@ -0,0 +1,12 @@
+package nl.danielle.cattery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatteryApplication.class, args);
+	}
+}
